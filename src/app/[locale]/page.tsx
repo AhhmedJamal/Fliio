dd713a96f560527ea0e4a92fe89d9c72d3337db8
ahhmed jamal <ahhmed.jamal@gmail.com>
2025-11-01 23:34:59 +0200

    update header

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,8 +3,8 @@ import HeroSection from "@/components/ui/HeroSection";
 
 export default function HomePage() {
   return (
-    <>
+    <div className="main-page">
       <HeroSection />
-    </>
+    </div>
   );
 }
