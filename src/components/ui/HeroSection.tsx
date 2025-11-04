aa89b5a4d0df71f2c0f95dfd530227d003277dbd
ahhmed jamal <ahhmed.jamal@gmail.com>
2025-11-04 23:21:15 +0200

    update header

--- src/components/ui/HeroSection.tsx
+++ src/components/ui/HeroSection.tsx
@@ -1,6 +1,12 @@
+"use clint"
 import { useTranslations } from "next-intl";
+import { useEffect } from "react";
 
 const HeroSection = () => {
+  
+  useEffect(()=>{
+
+  },[])
   const t = useTranslations("HeroSection");
   return <div className="p-10 text-center">{t("welcomeMessage")}</div>;
 };
---
+++ src/hooks/useDataTable.tsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { supabase } from "@/lib/supabase/config";
+
+type useDataError = string | null;
+
+export function useDataTable<T>(tableName: string) {
+  const [data, setData] = useState<T[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<useDataError>(null);
+
+  useEffect(() => {
+    if (!tableName) return;
+
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        const { data, error } = await supabase.from(tableName).select("*");
+        if (error) throw error;
+        if (data) setData(data as T[]);
+      } catch (err: unknown) {
+        if (err instanceof Error) setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [tableName]);
+
+  return { data, loading, error };
+}
