bd8165e5028adee826fd2ae303c22ac36bab5a43
Ahmed Jamal <ahhmed.jamal@gmail.com>
2025-11-30 23:02:48 +0200

    add component Image With Text

--- src/components/pages/home/ImageWithText.tsx
+++ src/components/pages/home/ImageWithText.tsx
@@ -6,11 +6,12 @@ import { ImageSquareItem } from "@/types/ImageSquare";
 import Image from "next/image";
 import React from "react";
 import { useSelector } from "react-redux";
+import { motion } from "framer-motion";
 
 const ImageWithText = () => {
   const { data, loading } = useDataTable<ImageSquareItem>("TextImage");
-
   const locale = useSelector((state: RootState) => state.locale.value);
+
   if (loading)
     return (
       <div className="flex flex-wrap justify-center gap-4 max-w-[70%] mx-auto my-11">
@@ -28,23 +29,33 @@ const ImageWithText = () => {
         </div>
       </div>
     );
+
   return (
-    <div className="flex items-center justify-center flex-wrap max-w-[70%] mx-auto my-11">
-      {data.map((item) => {
-        return (
-          <div key={item.id} className="flex items-center gap-4">
-            <p className="text-4xl">{item.text[locale]}</p>
-            {item.image && (
+    <div className="flex items-center justify-center flex-wrap gap-1 md:gap-4 md:max-w-[70%] mx-auto my-10">
+      {data.map((item) => (
+        <div key={item.id} className="flex items-center gap-4">
+          <p className="text-2xl md:text-4xl">{item.text[locale]}</p>
+          {item.image && (
+            <motion.div
+              whileHover={{
+                scale: [1, 1.1, 1.05],
+                x: [0, 5, -5, 0],
+                y: [0, -10, 0],
+              }}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+              className="w-20 md:w-36  relative"
+            >
               <Image
                 src={item.image || ""}
                 alt={item.text.en || ""}
-                height={150}
-                width={150}
+                height={100}
+                width={100}
+                className="w-20 md:w-36"
               />
-            )}
-          </div>
-        );
-      })}
+            </motion.div>
+          )}
+        </div>
+      ))}
     </div>
   );
 };
